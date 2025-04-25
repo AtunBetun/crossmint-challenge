16e5fb42a6cf6929c9976d22abec234815a73eb5
AtunBetun <beto7299@gmail.com>
2025-04-25 18:59:29 -0400

    save work

---
+++ src/CrossmintChallenge.Host/Clients/FlurlExtensions.cs
@@ -0,0 +1,37 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Flurl;
+using Flurl.Http;
+using Flurl.Http.Configuration;
+using Flurl.Http.Content;
+
+namespace CrossmintChallenge.Clients;
+
+public static class FlurlExtensions
+{
+    public static IFlurlRequest WithJsonSettings(this Url url) =>
+        url.WithSettings(settings =>
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower,
+            };
+            options.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+            settings.JsonSerializer = new DefaultJsonSerializer(options);
+        });
+
+    public static Task<IFlurlResponse> DeleteUrlEncodedAsync(
+        this IFlurlRequest request,
+        object body,
+        HttpCompletionOption completionOption = HttpCompletionOption.ResponseContentRead,
+        CancellationToken cancellationToken = default(CancellationToken)
+    )
+    {
+        request.EnsureClient();
+        CapturedUrlEncodedContent content = new CapturedUrlEncodedContent(
+            request.Settings.UrlEncodedSerializer.Serialize(body)
+        );
+        return request.SendAsync(HttpMethod.Delete, content, completionOption, cancellationToken);
+    }
+}
--- src/CrossmintChallenge.Host/Clients/MegaverseMapClient.cs
+++ src/CrossmintChallenge.Host/Clients/MegaverseMapClient.cs
@@ -4,26 +4,78 @@ using Serilog;
 
 namespace CrossmintChallenge.Clients;
 
+public record MapGoalResponse(List<List<GoalItem>> Goal);
+
 public enum GoalItem
 {
     SPACE,
     POLYANET,
 }
 
-public record MapResponse(List<List<GoalItem>> Goal);
+public record MapResponse(Map Map);
+
+public record Map(
+    string _id,
+    List<List<CellContent?>> Content,
+    string CandidateId,
+    int Phase,
+    int __v
+);
+
+public record CellContent(int Type);
 
 public static class MegaverseMapClient
 {
-    public static async Task<MapResponse> GetMap(Url challengeUrl, string candidateId)
+    public static async Task<MapGoalResponse> GetMapGoalAsync(Url challengeUrl, string candidateId)
     {
         Url url = challengeUrl
             .AppendPathSegments("map")
             .AppendPathSegments(candidateId)
             .AppendPathSegments("goal");
 
+        var result = await url.WithJsonSettings().GetJsonAsync<MapGoalResponse>();
+        Log.Debug("{@result}", result);
+        return result;
+    }
+
+    public static async Task<MapResponse> GetMapAsync(Url challengeUrl, string candidateId)
+    {
+        Url url = challengeUrl.AppendPathSegments("map").AppendPathSegments(candidateId);
         var result = await url.GetJsonAsync<MapResponse>();
         Log.Debug("{@result}", result);
-
         return result;
+    }
+
+    public static async Task DeletePolyanetAsync(
+        Url challengeUrl,
+        int row,
+        int column,
+        string candidateId
+    )
+    {
+        Url url = challengeUrl.AppendPathSegment("polyanets");
+        var formData = new Dictionary<string, string>
+        {
+            { "row", row.ToString() },
+            { "column", column.ToString() },
+            { "candidateId", candidateId },
+        };
+        var response = await url.WithHeader("Accept", "application/x-www-form-urlencoded")
+            .DeleteUrlEncodedAsync(formData);
+        Log.Debug("Deleted POLYANET at ({Row}, {Column}): {@Response}", row, column, response);
+    }
+
+    public static async Task PostPolyanetAsync(Url challengeUrl, int row, int column, string candidateId)
+    {
+        Url url = challengeUrl.AppendPathSegment("polyanets");
+        var formData = new Dictionary<string, string>
+        {
+            { "row", row.ToString() },
+            { "column", column.ToString() },
+            { "candidateId", candidateId },
+        };
+        var response = await url.WithHeader("Accept", "application/x-www-form-urlencoded")
+            .PostUrlEncodedAsync(formData);
+        Log.Debug("Posted POLYANET at ({Row}, {Column}): {@Response}", row, column, response);
     }
 }
